973c3772cd06e61f4124cb9c4b2a12e8bda2672d
Fathin Al Fikri <155558302+Alarick-Alfikry@users.noreply.github.com>
2024-06-16 17:06:46 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,70 @@
+import os, shutil, customtkinter
+from tkinter import *
+from tkinter import messagebox #Idk why the code still need this even though I previously imported * from ktinter ('^' ?)
+
+# Initialize Tkinter
+root = Tk()
+root.title("File Organizer")
+root.iconbitmap("image_content/icon.ico")
+root.geometry("800x500")
+root.resizable(False, False)
+
+bg = PhotoImage(file="image_content/background.png")
+bg_label = Label(root, image=bg)
+bg_label.place(x=0, y=0)
+
+# The function that will be executed when the button is clicked
+def organize_files():
+    # Gets the value from the input box
+    path = entry.get()
+    
+    if not os.path.exists(path):
+        messagebox.showerror("Error", "Path is invalid")
+    else:
+        files = os.listdir(path)
+
+        for item in files:
+            try:
+                filename, extension = os.path.splitext(item)
+                extension = extension[1:]
+
+                if os.path.exists(os.path.join(path, extension)):
+                    shutil.move(os.path.join(path, item), os.path.join(path, extension, item))
+                else:
+                    os.makedirs(os.path.join(path, extension))
+                    shutil.move(os.path.join(path, item), os.path.join(path, extension, item))
+
+            except FileNotFoundError:
+                messagebox.showerror("Error", "File not found")
+            except Exception as e:
+                messagebox.showerror("Error", f"Error: {e}")
+
+        messagebox.showinfo("Success", "Files organized successfully")
+
+# Create an input box (Entry)
+entry = customtkinter.CTkEntry(root, 
+        placeholder_text= "Enter Folder Path Here",
+        font= ("Helvetica", 20),
+        height= 44,
+        width= 354,
+        fg_color= "#FFF1E3",
+        bg_color= "#FFE2C7",
+        text_color= "#888888",
+        corner_radius= 30)
+entry.place(x=44, y=284)
+
+# Create a button to trigger the organize files function
+button = customtkinter.CTkButton(root, 
+        text= "Organize",
+        text_color="#FFFFFF",
+        font= ("Helvetica", 20),
+        height= 44,
+        width= 170,
+        fg_color= "#FF5678",
+        bg_color= "#FFE2C7",
+        hover_color= "#FF1342",
+        corner_radius= 30,
+        command= organize_files)
+button.place(x=136, y=343)
+
+root.mainloop()
